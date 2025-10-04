6e530061878fb8ca754fca27c901536fedc2f5e5
joshliford <j_liford1@yahoo.com>
2025-10-03 21:55:28 -0500

    added error handling for fetching posts

--- script.js
+++ script.js
@@ -14,21 +14,27 @@ fetchPromise.then(response => { // Grabs the response
 
         // Event Listener for 'Fetch Posts' button
         fetchButton.addEventListener("click", () => {
-            document.createElement("ul"); // Creates a list to display the posts
-
-            // Loop through each item (post) in the recieved JSON data
-            json.forEach(item => {
-                let postTitle = document.createElement("h2"); // Create an empty h2 element for the post title
-                let postBody = document.createElement("p"); // Create an empty p element for the post body
+            try {
+                document.createElement("ul"); // Creates a list to display the posts
+                // Loop through each item (post) in the recieved JSON data
+                json.forEach(item => {
+                    let postTitle = document.createElement("h2"); // Create an empty h2 element for the post title
+                    let postBody = document.createElement("p"); // Create an empty p element for the post body
                
-                postTitle.innerText = item.title; // add the JSON text to the title element
-                postBody.innerText = item.body; // add the JSON text to the body element
+                    postTitle.innerText = item.title; // add the JSON text to the title element
+                    postBody.innerText = item.body; // add the JSON text to the body element
 
-                // appends the JSON data to the existing postList div
-                postList.append(postTitle);
-                postList.append(postBody);
-            })
-        })
+                    // appends the JSON data to the existing postList div
+                    postList.append(postTitle);
+                    postList.append(postBody);
+                });
+            } catch (error) {
+                console.error("Error fetching posts:", error);
+            }
+        });
+
+
+        const submitButton = document.getElementById("submit");
 
         // Event Listner for 'Submit Post' button
         
