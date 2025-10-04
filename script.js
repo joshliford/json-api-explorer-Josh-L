234afb74b0fe026c89d81c9d1cdf8439cdceb900
joshliford <j_liford1@yahoo.com>
2025-10-03 21:42:44 -0500

    created an event listner on button click to fetch posts via a simulated API call

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-// so much empty
+// Wait for the page to load
+window.addEventListener("load", function() {
+
+//fetch request to the API URL
+const fetchPromise = fetch("https://jsonplaceholder.typicode.com/posts");
+
+fetchPromise.then(response => { // Grabs the response
+    const jsonPromise = response.json(); // Converts response into JSON format
+    jsonPromise.then(json => { // Grabs the JSON data
+        console.log(json);
+
+        const postList = document.getElementById("postList");
+        const fetchButton = document.getElementById("fetchButton");
+
+        // Event Listener for 'Fetch Posts' button
+        fetchButton.addEventListener("click", () => {
+            document.createElement("ul"); // Creates a list to display the posts
+
+            // Loop through each item (post) in the recieved JSON data
+            json.forEach(item => {
+                let postTitle = document.createElement("h2"); // Create an empty h2 element for the post title
+                let postBody = document.createElement("p"); // Create an empty p element for the post body
+               
+                postTitle.innerText = item.title; // add the JSON text to the title element
+                postBody.innerText = item.body; // add the JSON text to the body element
+
+                // appends the JSON data to the existing postList div
+                postList.append(postTitle);
+                postList.append(postBody);
+            })
+        })
+
+        // Event Listner for 'Submit Post' button
+        
+
+
+    });
+    });
+});
