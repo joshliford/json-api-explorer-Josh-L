0d3996f9c042e2156c6e9a9911ceccc9694f99d5
joshliford <j_liford1@yahoo.com>
2025-10-07 21:07:13 -0500

    added event listeners for fetching (GET) posts and submitting (POST) posts. also included error handling.

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// Wait for the page to load
+// wait for the page to load
 window.addEventListener("load", function() {
 
-//fetch request to the API URL
-const fetchPromise = fetch("https://jsonplaceholder.typicode.com/posts");
-
-fetchPromise.then(response => { // Grabs the response
-    const jsonPromise = response.json(); // Converts response into JSON format
-    jsonPromise.then(json => { // Grabs the JSON data
-        console.log(json);
+// fetch request to the API URL
+fetch("https://jsonplaceholder.typicode.com/posts").then(response => { // grab the response from the API
+    response.json().then((json) => { // convert the response into JSON format to use
 
         const postList = document.getElementById("postList");
         const fetchButton = document.getElementById("fetchButton");
 
         // Event Listener for 'Fetch Posts' button
         fetchButton.addEventListener("click", () => {
+            let loading = document.getElementById("loading");
+            loading.innerHTML = `<p>"Loading..."</p>`;
+            loading.style.display = 'flex'; // display a 'loading' message when the button is clicked
             try {
-                document.createElement("ul"); // Creates a list to display the posts
-                // Loop through each item (post) in the recieved JSON data
-                json.forEach(item => {
-                    let postTitle = document.createElement("h2"); // Create an empty h2 element for the post title
-                    let postBody = document.createElement("p"); // Create an empty p element for the post body
+                document.createElement("ul"); // creates a list to display the posts
+                json.forEach(item => { // loop through each item (post) in the recieved JSON data
+                    let postTitle = document.createElement("h2"); // create an empty h2 element for the post title
+                    let postBody = document.createElement("p"); // create an empty p element for the post body
                
                     postTitle.innerText = item.title; // add the JSON text to the title element
                     postBody.innerText = item.body; // add the JSON text to the body element
@@ -28,18 +26,36 @@ fetchPromise.then(response => { // Grabs the response
                     postList.append(postTitle);
                     postList.append(postBody);
                 });
-            } catch (error) {
+                loading.style.display = 'none'; // remove the 'loading' message when the data loads
+            } catch (error) { // Error handling
                 console.error("Error fetching posts:", error);
             }
         });
-
+    });
+});
 
         const submitButton = document.getElementById("submit");
-
+    
         // Event Listner for 'Submit Post' button
-        
+        submitButton.addEventListener("click", (event) => {
+            event.preventDefault(); // prevents the page from reloading when a post is submitted
+            const titleInput = document.getElementById("titleInput").value;
+            const bodyInput = document.getElementById("bodyInput").value;
+            // console.log("submit button clicked");
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify({
+                    titleInput,
+                    bodyInput,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
 
-
-    });
-    });
+            const formSuccess = document.getElementById("formSuccess");
+            formSuccess.innerText = `Post sucessfully uploaded. Post title: ${titleInput}`; // display a message when a post is "created"
+        })
 });
